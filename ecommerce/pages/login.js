8a9a3013c8e9b0e414a1a47815e299c95c3a6cb7
AlehCriz <cristian.sandoval46@inacapmail.cl>
2023-07-14 05:37:15 -0400

    first commit

---
+++ ecommerce/pages/login.js
@@ -0,0 +1,93 @@
+import Link from 'next/link';
+import React, { useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import Layout from '../components/Layout';
+import { getError } from '../utils/error';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
+
+export default function LoginScreen() {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  const { redirect } = router.query;
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push(redirect || '/');
+    }
+  }, [router, session, redirect]);
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const submitHandler = async ({ email, password }) => {
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        email,
+        password,
+      });
+      if (result.error) {
+        toast.error(result.error);
+      }
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+  return (
+    <Layout title="Login">
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-4 text-xl">Ingresar</h1>
+        <div className="mb-4">
+          <label htmlFor="email">Correo</label>
+          <input
+            type="email"
+            {...register('email', {
+              required: 'Por favor ingrese su email',
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: 'Please enter valid email',
+              },
+            })}
+            className="w-full"
+            id="email"
+            autoFocus
+          ></input>
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password">Contraseña</label>
+          <input
+            type="password"
+            {...register('password', {
+              required: 'Por favor ingrese su contraseña',
+              minLength: { value: 6, message: 'la contraseña debe de tener mas de 5 caracteres' },
+            })}
+            className="w-full"
+            id="password"
+            autoFocus
+          ></input>
+          {errors.password && (
+            <div className="text-red-500 ">{errors.password.message}</div>
+          )}
+        </div>
+        <div className="mb-4 ">
+          <button className="primary-button">Iniciar</button>
+        </div>
+        <div className="mb-4 ">
+        ¿No tienes un cuenta? &nbsp;
+          <Link href={`/register?redirect=${redirect || '/'}`}>Registrate</Link>
+        </div>
+      </form>
+    </Layout>
+  );
+}
